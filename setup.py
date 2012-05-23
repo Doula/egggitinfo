7283c206d4170a738f3cacad78b0ef71b2f7bdcf
Joe Dallago <joed@surveymonkey.com>
2012-05-23 10:01:12 -0700

    Added gitdb as a dependency.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
-requires = ['gitpython']
+requires = ['gitpython', 'gitdb']
 
 setup(name='egggitinfo',
       version='0.1',
