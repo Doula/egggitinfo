6f5efa3d0ba0bfc7999c2aa2510a27da2ab4b998
Joe Dallago <joed@surveymonkey.com>
2012-05-23 10:00:05 -0700

    Cleaned up the README a bit.  Also got the package installing correctly from pypi.

--- README.rst
+++ README.rst
@@ -1,6 +1,9 @@
+================
+egggitinfo README
+=================
+
 Overview
 ========
-
 This is a setuptools plugin that plants some git-related information into the .egg-info directory of a python package.  To setuptools this information is refered to as 'metadata'.  It is located in a file called 'git_info.txt', and can be retrieved using the from a Distribution object by calling the `get_metadata(name)` method.  The 'git_info.txt' file stores the following information in JSON format.
 
 git_branch
--- setup.py
+++ setup.py
@@ -11,13 +11,12 @@ setup(name='egggitinfo',
       version='0.1',
       description='Plants some git-related data into the .egg-info directory of a python package.',
       long_description=README,
-      author='',
-      author_email='',
-      url='',
+      py_modules = ['egggitinfo'],
       install_requires=requires,
       test_suite="egggitinfo",
-      entry_points = """\
-      [egg_info.writers]
-      git_info.txt = egggitinfo:write_git_info
-      """,
-      )
+      entry_points = {
+          "egg_info.writers": [
+              "git_info.txt = egggitinfo:write_git_info",
+          ],
+      },
+)
